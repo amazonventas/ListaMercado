d69b8611c7cd63050b823045b4ef41169efad513
amazonventas <electricidadfacil80@gmail.com>
2024-12-05 10:15:17 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -17,61 +17,77 @@ if (!('webkitSpeechRecognition' in window)) {
     console.log("Iniciando reconocimiento de voz...");
   });
 
+
   recognition.onresult = function(event) {
-    // La transcripción resultante se obtiene en event.results
+
+     // La transcripción resultante se obtiene en event.results
     const transcript = event.results[0][0].transcript;
+
+    // Separa el parrafo en
     const Separadas = transcript.split(" ");
+
     //textOutput.textContent = "Texto reconocido: " + transcript ; // Mostramos el texto transcrito
     var N = Separadas.length;
-    console.log(transcript);
-    console.log(Separadas);
-    console.log(N);
+
+    
+    var botones = document.querySelectorAll("button");
+    var Cantidad = botones.length - 1;
 
     for (var i = 0; i < N; i++) {
-
-      console.log(Separadas[i]);
-      agregarFila(Separadas[i],i);
+      agregarFila(Separadas[i],i, Cantidad);
     }
 
-    var botones = document.querySelectorAll("input");
-    console.log(botones);
-    
+    console.log(Cantidad);
+
+    var botones = document.querySelectorAll("button");
+
     botones.forEach(function(boton) {
       boton.addEventListener("click", function(event) {
           // El objeto event.target es el elemento que fue clicado
-          console.log("Se ha clicado el elemento:", event.target);
-          console.log("ID del elemento clicado:", event.target.id);
-
-          var Fila = document.getElementById("Fila" + event.target.id);
-          var Marcado = document.getElementById(event.target.id).checked;
+          //console.log("Se ha clicado el elemento:", event.target);
+          //console.log("ID del elemento clicado:", event.target.id);
 
 
-          if ( Marcado == true) {
-            Fila.style.color = "red";
-          } else {
-            Fila.style.color = "black";
-          } 
-
-      
         });
     });
-    
 
+
+      // Obtener todas las filas con el botón "Eliminar"
+      const botonesEliminar = document.querySelectorAll('.Eliminar');
+  
+      console.log(botonesEliminar);
+  
+      // Iterar sobre cada botón y agregar un evento de clic
+      botonesEliminar.forEach(boton => {
+        boton.addEventListener('click', function() {
+
+          let respuesta = confirm("¿Estás seguro que quieres Borrar?");
+          if (respuesta) {
+           // Obtener la fila en la que se hizo clic
+           const fila = this.parentNode.parentNode.parentNode; // el botón está dentro de una celda <td>, que a su vez está dentro de una fila <tr>
+           fila.remove(); // Eliminar la fila
+          }
+          
+        });
+      });
+    
   };
 
   recognition.onerror = function(event) {
     // Si ocurre un error, lo mostramos en consola
     console.error("Error en el reconocimiento de voz: ", event.error);
   };
-
+  
   recognition.onend = function() {
     console.log("El reconocimiento de voz ha terminado.");
   };
-}
+
+};
 
 
 
-function agregarFila(Articulo, Identificador) {
+function agregarFila(Articulo, Identificador ,Cantidad) {
+
   // Obtén el cuerpo de la tabla
   var tabla = document.getElementById("tabla").getElementsByTagName('tbody')[0];
 
@@ -79,14 +95,15 @@ function agregarFila(Articulo, Identificador) {
   var nuevaFila = tabla.insertRow();
 
   // Crea las celdas para la nueva fila
-  var celdaNombre = nuevaFila.insertCell(0);
-  var celdaAcciones = nuevaFila.insertCell(1);
-      
-  celdaNombre.id = "Fila" + Identificador;
+  var celdaNombre   = nuevaFila.insertCell(0);
+  var celdaBorrar   = nuevaFila.insertCell(1);
+  
+  var Total_id = Identificador + Cantidad
 
-  // Asigna valores a las celdas
-  celdaNombre.innerHTML = Articulo;
-  celdaAcciones.innerHTML = '<input id =' + Identificador + ' type="checkbox">';
+  celdaNombre.id = "Fila" + Total_id;
+
+  celdaNombre.innerHTML   ='<center>'+Articulo+'</center>';
+  celdaBorrar.innerHTML   ='<center><button Class="Eliminar" id=Borrar' + Total_id +'>borrar</button></center>';
 
 
 }
