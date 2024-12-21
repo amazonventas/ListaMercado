f9b57f96cb9d5aad2b4cc2816f05ce38cbe6c6b5
amazonventas <electricidadfacil80@gmail.com>
2024-12-20 22:01:39 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -163,33 +163,3 @@ Body1.addEventListener('click', function(event) {
 });
 
 
-Archivo_JSON.addEventListener('click', function() {
-  Guardar_JSON();
-});
-
-
-function Guardar_JSON(){
-
-// Obtener la lista del localStorage
-let listaRecuperada = JSON.parse(localStorage.getItem("Listado"));
-
-// Verifica si la lista existe
-if (listaRecuperada) {
-    // Convertir la lista a JSON
-    let datos = JSON.stringify(listaRecuperada, null, 2);
-
-    // Crear un Blob con los datos de la lista
-    let blob = new Blob([datos], { type: 'application/json' });
-
-    // Crear un enlace para la descarga
-    let enlace = document.createElement('a');
-    enlace.href = URL.createObjectURL(blob);
-    enlace.download = 'miLista.json';  // El nombre del archivo descargado
-
-    // Simular un clic para descargar el archivo
-    enlace.click();
-} else {
-    console.log("No hay lista en localStorage.");
-}
-
-}
